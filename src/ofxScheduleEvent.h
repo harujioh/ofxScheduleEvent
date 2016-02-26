215e5d1d75a98392cc317911fc5738546070b415
hirata <hirata@team-lab.com>
2016-02-27 00:09:47 +0900

    デストラクタでスレッド解放

--- src/ofxScheduleEvent.h
+++ src/ofxScheduleEvent.h
@@ -4,18 +4,25 @@
 
 class ofxScheduleEvent : public ofThread {
    public:
-    ofxScheduleEvent(int interval = 1);
+    // constructor, destructor
+    ofxScheduleEvent(int interval = 1) : interval(max(0, interval)) {}
+    ~ofxScheduleEvent();
 
+    // add schedule event
     void addSchedule(int hour, int minute);
     void addSchedule(int minute);
 
+    // thread main
     void threadedFunction();
-    void update();
 
+    // event handler
     ofEvent<void> onEvent;
 
    private:
     int interval;
     vector<int> times;
     int lastCheckAppExitMin;
+
+    // thread update
+    void threadedUpdate();
 };
